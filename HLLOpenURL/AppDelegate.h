06d851c12f888a1d59faac19dbd0259d36e922e7
rocky <983272765@qq.com>
2015-08-27 15:08:20 +0800

    Initial Commit

---
+++ HLLOpenURL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLLOpenURL
+//
+//  Created by apple on 15/8/27.
+//  Copyright (c) 2015年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLLOpenURL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLLOpenURL
+//
+//  Created by apple on 15/8/27.
+//  Copyright (c) 2015年 HLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
